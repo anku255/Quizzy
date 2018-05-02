cb496a1c99f2629d4e2f464143d17cba8d7a3743
Ankit Tiwari <ankitt255@gmail.com>
2018-05-02 22:32:22 +0530

    📝 Add initial code for client side

---
+++ client/src/Components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div>Hello, World! This is going to be Quizzy!!</div>;
+  }
+}
+
+export default App;
