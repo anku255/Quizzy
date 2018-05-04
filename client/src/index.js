811ec811bf490579e75b7433f2df3e77cc1db50d
Ankit Tiwari <ankitt255@gmail.com>
2018-05-04 11:25:36 +0530

    ⚙ Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import './index.css';
 import App from './Components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
