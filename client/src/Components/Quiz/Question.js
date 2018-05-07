5e9309ea509967119f92bf031e386b58f5ec9e35
Ankit Tiwari <ankitt255@gmail.com>
2018-05-07 18:55:57 +0530

    📝Add a separate component for Question in Quiz component

---
+++ client/src/Components/Quiz/Question.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import Radio, { RadioGroup } from 'material-ui/Radio';
+import { FormControl, FormLabel, FormControlLabel } from 'material-ui/Form';
+
+// CSS
+const styles = {
+  gridContainer: {
+    textAlign: 'center'
+  },
+  paper: {
+    textAlign: 'left',
+    padding: 16,
+    marginTop: 5
+  },
+  submitBtn: {
+    color: 'white'
+  }
+};
+
+export default props => {
+  return props.currentQuiz.map((Quiz, quesIndex) => {
+    return (
+      <Grid key={Quiz._id} item xs={12}>
+        {/* One Grid Item consist of One Paper Component */}
+        <Paper style={styles.paper} elevation={4}>
+          {/* Question Text (h4) */}
+          <Typography variant="headline" component="h4">
+            {Quiz.text}
+          </Typography>
+
+          {/* Choices are wrapped inside FromControl Component */}
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Options:</FormLabel>
+
+            {/* Radio buttons are grouped within a RadioGroup */}
+            <RadioGroup
+              onChange={props.handleChange}
+              value={props.radioBtnValue[quesIndex]}
+            >
+              {Quiz.choices.map((choice, index) => {
+                return (
+                  <FormControlLabel
+                    key={choice}
+                    value={`${Quiz._id} ${index} ${quesIndex}`}
+                    control={<Radio />}
+                    label={choice}
+                  />
+                );
+              })}
+            </RadioGroup>
+          </FormControl>
+        </Paper>
+      </Grid>
+    );
+  });
+};
--- client/src/Components/Quiz/Quiz.js
+++ client/src/Components/Quiz/Quiz.js
@@ -3,25 +3,14 @@ import * as actions from '../../actions';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
-import Typography from 'material-ui/Typography';
-import Radio, { RadioGroup } from 'material-ui/Radio';
-import { FormControl, FormLabel, FormControlLabel } from 'material-ui/Form';
 import Button from 'material-ui/Button';
+import Question from './Question';
 
 // CSS
 const styles = {
   root: {
     margin: '0 2%',
     flexGrow: 1
-  },
-  gridContainer: {
-    textAlign: 'center'
-  },
-  paper: {
-    textAlign: 'left',
-    padding: 16,
-    marginTop: 5
   },
   submitBtn: {
     color: 'white'
@@ -64,30 +53,11 @@ class Quiz extends Component {
     return <div style={styles.root}>
         {/* Grid Container */}
         <Grid container style={styles.gridContainer}>
-          {this.props.currentQuiz.map((Quiz, quesIndex) => {
-            return <Grid key={Quiz._id} item xs={12}>
-                {/* One Grid Item consist of One Paper Component */}
-                <Paper style={styles.paper} elevation={4}>
-                  {/* Question Text (h4) */}
-                  <Typography variant="headline" component="h4">
-                    {Quiz.text}
-                  </Typography>
-
-                  {/* Choices are wrapped inside FromControl Component */}
-                  <FormControl component="fieldset">
-                    <FormLabel component="legend">Options:</FormLabel>
-
-                    {/* Radio buttons are grouped within a RadioGroup */}
-                    <RadioGroup onChange={this.handleChange} value={this.state.radioBtnValue[quesIndex]}>
-                      {Quiz.choices.map((choice, index) => {
-                        return <FormControlLabel key={choice} value={`${Quiz._id} ${index} ${quesIndex}`} control={<Radio />} label={choice} />;
-                      })}
-                    </RadioGroup>
-                  </FormControl>
-                </Paper>
-              </Grid>;
-          })}
-
+          <Question
+            currentQuiz={this.props.currentQuiz}
+            radioBtnValue={this.state.radioBtnValue}
+            handleChange={this.handleChange}
+          />
           <Button variant="raised" color="primary">
             <Link to={{
               pathname: '/current/answers',
