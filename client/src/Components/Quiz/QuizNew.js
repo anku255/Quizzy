9b341b1ded62ba0d777aedc6e508663e5502b727
Ankit Tiwari <ankitt255@gmail.com>
2018-05-13 19:01:17 +0530

    📝 Add Bulma designed Quiz and Question component

---
+++ client/src/Components/Quiz/QuizNew.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import { Link } from 'react-router-dom';
+import QuestionNew from './QuestionNew';
+
+const classes = {
+  isCentered: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+};
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      response: {}
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  componentDidMount = () => {
+    this.props.fetchCurrentQuiz();
+  };
+
+  handleInputChange(event) {
+    const choiceIndex = event.target.getAttribute('data-choice-index');
+    const quesId = event.target.getAttribute('data-question-id');
+
+    this.setState(prevState => {
+      return {
+        response: { ...prevState.response, [quesId]: choiceIndex }
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="container" style={{ margin: '20px auto' }}>
+        <QuestionNew
+          currentQuiz={this.props.currentQuiz}
+          handleInputChange={this.handleInputChange}
+        />
+        <div style={classes.isCentered}>
+          <Link
+            className="button is-primary"
+            to={{
+              pathname: '/current/answers',
+              state: {
+                response: this.state.response,
+                currentQuiz: this.props.currentQuiz
+              }
+            }}
+          >
+            Submit
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    currentQuiz: state.quiz.currentQuiz,
+    loading: state.quiz.loading
+  };
+}
+
+export default connect(mapStateToProps, actions)(Quiz);
