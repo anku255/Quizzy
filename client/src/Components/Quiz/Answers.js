ec4aacf03c4f9032eded18b4020476c386a5b7bd
Ankit Tiwari <ankitt255@gmail.com>
2018-05-15 12:29:56 +0530

    📝Add a method to send quiz response to the server

--- client/src/Components/Quiz/Answers.js
+++ client/src/Components/Quiz/Answers.js
@@ -11,6 +11,41 @@ class Answers extends Component {
       response: this.props.location.state.response
     };
   }
+  /**
+   * @returns an array of objects. Each object of
+   * the form {quesId, correctAnswer, incorrectAnswer}.
+   *
+   * This array of object will be sent to server.
+   */
+  evalResponse() {
+    const result = [];
+    const currentQuiz = this.state.currentQuiz;
+    const response = this.state.response;
+    for (let i = 0; i < currentQuiz.length; i++) {
+      let question = currentQuiz[i];
+      let correctAnswer = question.correctAnsIndex === +response[question._id];
+      let incorrectAnswer =
+        question.correctAnsIndex !== +response[question._id];
+
+      result.push({
+        quesId: question._id,
+        category: question.category,
+        correctAnswer,
+        incorrectAnswer
+      });
+    }
+
+    return result;
+  }
+
+  componentDidMount = () => {
+      const result = this.evalResponse();
+      // call the action for sending response to server
+      this.props.submitQuizResponse(result);
+      this.setState({
+        responseSubmitted: true
+      });
+  };
 
   render() {
     return (
