58a0e37ef801cadcc9687bfb83881b9139ae66ae
Ankit Tiwari <ankitt255@gmail.com>
2018-05-15 15:39:48 +0530

    📝Remove material-ui

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import './css/mystyles.css';
 import './utils/fontawesome/js/fontawesome-all.min.js';
 import './index.css';
@@ -12,13 +11,10 @@ import reducers from './reducers';
 import App from './Components/App';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-const theme = createMuiTheme();
 
 ReactDOM.render(
   <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
-      <App />
-    </MuiThemeProvider>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
